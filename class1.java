e7f694ddeb45f08d6e290918b2baebe084fe1f0c
maryam <mbabaei_26@yahoo.com>
2023-11-24 08:28:41 -0800

    hello world java program

---
+++ class1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class class1 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("------hello world!------");
+        System.out.println("\nwhere are you now??");
+        String Place=input.next();
+        System.out.println("hello from Iran to "+ Place);
+    }
+}
+
+
+
+
